093fce74c08c0789424fa10023d7dc96346a2dfb
mrsheen <info@mrsheenau.com>
2010-09-28 21:24:38 +1000

    Borked up the onclick handler for markers

--- webpage/map.js
+++ webpage/map.js
@@ -177,12 +177,15 @@
         map: map,
         title: item.msg
 
-      });
-          markers[item.msg+item.id] = marker;
-          $('#mcmarkerlist div[name=mcmarkers'+item.id+']').append('<div class="mcmarker" name="'+item.msg+item.id+'">'+item.msg+'</div>').click(function() {
-                        map.panTo(markers[$(this).attr("name")].getPosition());
-                        map.setZoom(config.markerZoom);
-                });
+		});
+        markers[item.msg+item.id] = marker;
+		  
+		$('#mcmarkerlist div[name='+item.msg+item.id+']').append('<div class="mcmarker" name="'+item.msg+item.id+'">'+item.msg+'</div>');
+
+        $('#mcmarkerlist div[name=mcmarkers'+item.id+'] div[name="'+item.msg+item.id+']').click(function() {
+				map.panTo(markers[$(this).attr("name")].getPosition());
+                map.setZoom(config.markerZoom);
+			});
           }
    }
 
