d69b2764d232a6f98f67d0b73d66cd41ff84cef7
Alejandro Aguilera <fenixin@lavabit.com>
2010-12-15 14:55:25 +0100

    Fix chunk image size in chunk.py

--- chunk.py
+++ chunk.py
@@ -517,7 +517,7 @@ class ChunkRenderer(object):
         # Since there are 16x16x128 blocks in a chunk, the image will be 384x1728
         # (height is 128*12 high, plus the size of the horizontal plane: 16*12)
         if not img:
-            img = Image.new("RGBA", (384, 1728), (38,92,255,0))
+            img = Image.new("RGBA", (384, 1746), (38,92,255,0))
 
         for x,y,z,imgx,imgy in iterate_chunkblocks(xoff,yoff):
             blockid = blocks[x,y,z]
