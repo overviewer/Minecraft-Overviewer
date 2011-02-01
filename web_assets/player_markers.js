3ca6c4e087339886a370a7b25f7240891b90a251
FabianN <FabianN@gmail.com>
2011-01-31 23:56:33 -0800

    Fixed player_markers.js not pulling up the correct image.

--- web_assets/player_markers.js
+++ web_assets/player_markers.js
@@ -14,7 +14,7 @@ setInterval(loadPlayerMarkers, 1000 * 15);
 setTimeout(loadPlayerMarkers, 1000);
 
 function preparePlayerMarker(marker,item) {
-	var c = "<div class=\"infoWindow\" style='width: 300px'><img src='player-avatar.php?player="+item.msg"&s=5&bc=000&bw=2&format=flat'/><h1>"+item.msg+"</h1></div>";
+	var c = "<div class=\"infoWindow\" style='width: 300px'><img src='player-avatar.php?player="+item.msg+"&s=3&bc=000&bw=1&format=flat'/><h1>"+item.msg+"</h1></div>";
 	var infowindow = new google.maps.InfoWindow({content: c});
 	google.maps.event.addListener(marker, 'click', function() {
 		infowindow.open(map,marker);
@@ -32,7 +32,7 @@ function loadPlayerMarkers() {
 				position: converted,
 				map: map,
 				title: item.msg,
-				icon: 'player-avatar.php?player='. +item.msg .'&s=1&bc=fff&bw=1&format=flat',
+				icon: "player-avatar.php?player="+item.msg+"&s=1&bc=fff&bw=1&format=flat",
 				visible: true,
 				zIndex: 999
 			});
