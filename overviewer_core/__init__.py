14f68ebeb79d6d07f7ac69cb6380cdf60a9f022e
Aaron Griffith <aargri@gmail.com>
2012-01-07 20:29:46 -0500

    import in __init__.py no longer prevents setup.py from working

--- overviewer_core/__init__.py
+++ overviewer_core/__init__.py
@@ -1,5 +1,7 @@
 # c_overviewer must be imported first, because it imports other
 # modules; leaving this out can lead to bad dependency loops
 
-import c_overviewer
-
+try:
+    import c_overviewer
+except ImportError:
+    pass
