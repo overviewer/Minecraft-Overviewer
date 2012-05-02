e69717f476920a3a38b350c36bdfaaddc1d909c8
Fabian Norman <FabianN@gmail.com>
2012-05-02 00:43:53 -0700

    Prevent null being set in overviewerConfig.js

--- overviewer_core/settingsDefinition.py
+++ overviewer_core/settingsDefinition.py
@@ -80,7 +80,7 @@ renders = Setting(required=True, default=util.OrderedDict(),
             "changelist": Setting(required=False, validator=validateStr, default=None),
             "markers": Setting(required=False, validator=validateMarkers, default=[]),
             "showspawn": Setting(required=False, validator=validateBool, default=True),
-            "base": Setting(required=False, validator=validateStr, default=None),
+            "base": Setting(required=False, validator=validateStr, default=""),
 
             # Remove this eventually (once people update their configs)
             "worldname": Setting(required=False, default=None,
