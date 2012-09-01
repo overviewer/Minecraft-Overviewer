4c29cf453d3e48f20d525ee5ce22cf7028ad3236
Socolin <beber-le-scaraber@hotmail.fr>
2012-09-01 15:44:09 +0300

    Support blockid > 255

--- overviewer_core/src/overviewer.h
+++ overviewer_core/src/overviewer.h
@@ -136,7 +136,7 @@ extern unsigned int max_blockid;
 extern unsigned int max_data;
 extern unsigned char *block_properties;
 static inline int
-block_has_property(unsigned char b, BlockProperty prop) {
+block_has_property(unsigned short b, BlockProperty prop) {
     if (b >= max_blockid || !(block_properties[b] & (1 << KNOWN))) {
         /* block is unknown, return defaults */
         if (prop == TRANSPARENT)
