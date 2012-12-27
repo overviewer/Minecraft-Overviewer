aaff40146a3b580b64f8046384fd2bf9e2db1adf
Aaron Griffith <aargri@gmail.com>
2012-12-27 14:27:12 -0500

    test.py is now suitable for testing many backends at once

--- test.py
+++ test.py
@@ -6,12 +6,12 @@ import OIL
 import StringIO
 import random
 
-def time_test(func, timelimit=10.0):
+def time_test(func, arg, timelimit=10.0):
     times = []
     last_time = start_time = time.time()
     while last_time - start_time < timelimit:
         random.seed(0)
-        func()
+        func(arg)
         cur_time = time.time()
         times.append(cur_time - last_time)
         last_time = cur_time
@@ -20,51 +20,50 @@ def time_test(func, timelimit=10.0):
     stddev = (sum([(i - mean)**2 for i in times]) / (count - 1))**0.5
     return (count, last_time - start_time, mean, stddev)
 
-pilim = PIL.Image.open("input.png")
-pilim.load()
-oilim = OIL.Image.load("input.png")
+image_paths = {
+    'input' : 'input.png',
+    'dice' : 'dice.png',
+}
 
-pildice = PIL.Image.open("dice.png")
-pildice.load()
-oildice = OIL.Image.load("dice.png")
+images = {}
 
 NUM_COMPOSITES = 40
 
-def pil_composite():
+def pil_composite(out):
     dest = PIL.Image.new("RGBA", (512, 512))
     for _ in xrange(NUM_COMPOSITES):
         dx = random.randint(0, 511)
         dy = random.randint(0, 511)
-        dest.paste(pildice, (dx, dy), pildice)
-    dest.save("pilcomposite.png")
+        dest.paste(images['dice'], (dx, dy), images['dice'])
+    dest.save(out)
 
-def oil_composite():
+def oil_composite(out):
     dest = OIL.Image(512, 512)
     for _ in xrange(NUM_COMPOSITES):
         dx = random.randint(0, 511)
         dy = random.randint(0, 511)
-        dest.composite(oildice, 255, dx, dy, 0, 0, 0, 0)
-    dest.save("oilcomposite.png")
+        dest.composite(images['dice'], 255, dx, dy, 0, 0, 0, 0)
+    dest.save(out)
 
 tests = [
-    ("Loading", [
-            ("PIL", None, lambda: PIL.Image.open("input.png").load()),
-            ("OIL", None, lambda: OIL.Image.load("input.png")),
+    ("Load", [
+            ("PIL", None, lambda o: PIL.Image.open(image_paths['input']).load()),
+            ("OIL", "CPU", lambda o: OIL.Image.load(image_paths['input'])),
     ]),
     
-    ("Saving", [
-            ("PIL", "pil.png", lambda: pilim.save("pil.png")),
-            ("OIL", "oil.png", lambda: oilim.save("oil.png")),
+    ("Save", [
+            ("PIL", None, lambda o: images['input'].save(o)),
+            ("OIL", "CPU", lambda o: images['input'].save(o)),
     ]),
 
-    ("Saving (Palette)", [
-            ("PIL", "pilpalette.png", lambda: pilim.convert('RGB').convert('P', palette=PIL.Image.ADAPTIVE, colors=256).save("pilpalette.png")),
-            ("OIL", "oilpalette.png", lambda: oilim.save("oilpalette.png", indexed=True, palette_size=256)),
+    ("SaveDither", [
+            ("PIL", None, lambda o: images['input'].convert('RGB').convert('P', palette=PIL.Image.ADAPTIVE, colors=256).save(o)),
+            ("OIL", "CPU", lambda o: images['input'].save(o, indexed=True, palette_size=256)),
     ]),
     
-    ("Compositing", [
-            ("PIL", "pilcomposite.png", pil_composite),
-            ("OIL", "oilcomposite.png", oil_composite),
+    ("Composite", [
+            ("PIL", None, pil_composite),
+            ("OIL", "CPU", oil_composite),
     ]),
 ]
 
@@ -109,21 +108,47 @@ def draw_bar(offset, length, bound_low, bound_high, start, end, seq):
     file.write("\n")
 
 for name, testlist in tests:
-    table = Table(20, 5, 17, 19, 10, 5)
-    table.column(name + ":", "name", "seconds per call", "standard deviation", "file size", "(%)")
-    table.column("",         "----", "----------------", "------------------", "---------", "---")
+    print name + ":"
+    print
+    table = Table(4, 5, 17, 19, 5, 10, 5)
+    table.column("", "name", "seconds per call", "standard deviation", "(%)", "file size", "(%)")
+    table.column("", "----", "----------------", "------------------", "---", "---------", "---")
     bars = []
+    reference_time = None
     reference_size = None
-    for case, fname, func in testlist:
-        _, _, percall, stddev = time_test(func)
+    for case, backend, func in testlist:
+        if backend:
+            backend_id = getattr(OIL, "BACKEND_" + backend)
+            OIL.backend_set(backend_id)
+            for key, path in image_paths.items():
+                images[key] = OIL.Image.load(path)
+        else:
+            for key, path in image_paths.items():
+                images[key] = PIL.Image.open(path)
+        
+        fname = "./test/" + case
+        if backend:
+            fname += " (" + backend + ")"
+        fname += " " + name
+        fname += ".png"
+        
+        _, _, percall, stddev = time_test(func, fname)
+        if reference_time is None:
+            reference_time = percall
+        percallper = str(float(percall) / reference_time)
+        percallper = percallper[:4]
+
         fsize = "--"
         fper = ""
         if fname:
-            fsize = os.stat(fname).st_size
-            if reference_size is None:
-                reference_size = fsize
-            fper = float(fsize) / reference_size
-        table.column("", case, percall, stddev, fsize, fper)
+            try:
+                fsize = os.stat(fname).st_size
+                if reference_size is None:
+                    reference_size = fsize
+                fper = float(fsize) / reference_size
+            except OSError:
+                pass
+        table.column("", case, percall, stddev, percallper, fsize, fper)
         bars.append((case, percall, stddev))
     print
     bounds = []
