2b78b63e1a0340aeada11ac02f8156c9f9c68b0d
Nicolas Frattaroli <kleinnici@frattaroli.ch>
2013-11-08 06:56:45 +0100

    Fixed IE11 JS errors
    Closes issue #1022.
    Thanks to mattlee78 for pointing this out.

--- overviewer_core/data/js_src/models.js
+++ overviewer_core/data/js_src/models.js
@@ -23,14 +23,14 @@ overviewer.models.WorldCollection = Backbone.Collection.extend({
  */
 overviewer.models.TileSetModel = Backbone.Model.extend({
     defaults: {
-        markers: [] ,
+        markers: []
     },
     initialize: function(attrs) {
         // this implies that the Worlds collection must be
         // initialized before any TIleSetModels are created
         attrs.world = overviewer.collections.worlds.get(attrs.world);
         this.set(attrs);
-    },
+    }
 });
 
 overviewer.models.TileSetCollection = Backbone.Collection.extend({
@@ -42,6 +42,6 @@ overviewer.models.GoogleMapModel = Backbone.Model.extend({
     initialize: function(attrs) {
         attrs.currentWorldView = overviewer.collections.worldViews[0];
         this.set(attrs);
-    },
+    }
 });
 
