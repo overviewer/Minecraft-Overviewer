cf695ed9de944bbff91d1bbb01adadc13a1e56c4
Ben Steadman <steadmanben1@gmail.com>
2019-03-27 20:04:14 +0000

    regionTrimmer.py tests

--- test/test_all.py
+++ test/test_all.py
@@ -18,6 +18,7 @@ from test_cache import TestLRU
 from test_contributors import TestContributors
 from test_cyrillic_convert import TestCyrillicConvert
 from test_playerInspect import TestPlayerInspect
+from test_regionTrimmer import TestRegionTrimmer
 
 # DISABLE THIS BLOCK TO GET LOG OUTPUT FROM TILESET FOR DEBUGGING
 if 0:
