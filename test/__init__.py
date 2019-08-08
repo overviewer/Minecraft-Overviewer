bdd3b7bc166d6aac86b0d80791e3cc0d073daead
Nicolas F <ovdev@fratti.ch>
2019-08-08 21:31:41 +0200

    test: import relative to __init__.py
    Might fix tests for Debian builders somehow maybe?

--- test/__init__.py
+++ test/__init__.py
@@ -2,5 +2,4 @@
 import sys
 import os
 import os.path
-sys.path.insert(0, os.getcwd())
-sys.path.insert(0, os.path.join(os.getcwd(), os.pardir))
+sys.path.insert(0, os.path.join(os.path.split(__file__)[0], '..'))
