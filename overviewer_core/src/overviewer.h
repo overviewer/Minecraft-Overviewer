ab0f6ab3882ee8d10cfab04726bf444fa04afb20
Joseph Camp <josephcamp1992@gmail.com>
2020-06-22 21:46:56 +0000

    overviewer.h: Bump extension version

--- overviewer_core/src/overviewer.h
+++ overviewer_core/src/overviewer.h
@@ -31,7 +31,7 @@
 
 // increment this value if you've made a change to the c extension
 // and want to force users to rebuild
-#define OVERVIEWER_EXTENSION_VERSION 84
+#define OVERVIEWER_EXTENSION_VERSION 85
 
 #include <stdbool.h>
 #include <stdint.h>
