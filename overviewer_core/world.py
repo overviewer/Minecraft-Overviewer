5596cd980995d9670051667f44c8ab3ff1a420e0
Thomas Howard <thomas.howard@education.govt.nz>
2023-06-16 12:58:54 +1200

    Expanded list of statuses gathered from looking at the Bluemap code
    This can be done better, once I work out how to to it better. This does get rendering of 1.20 touched chunks running again however.

--- overviewer_core/world.py
+++ overviewer_core/world.py
@@ -1688,8 +1688,8 @@ class RegionSet(object):
         # Empty is self-explanatory, and liquid_carved and carved seem to correspond
         # to SkyLight not being calculated, which results in mostly-black chunks,
         # so we'll just pretend they aren't there.
-        if chunk_data.get("Status", "") not in ("full", "postprocessed", "fullchunk",
-                                                "mobs_spawned", "spawn", ""):
+        if chunk_data.get("Status", "") not in ("minecraft:full", "full", "minecraft:postprocessed" ,"postprocessed", "minecraft:fullchunk", "fullchunk",
+                                                "minecraft:mobs_spawned", "mobs_spawned", "minecraft:spawn", "spawn", ""):
             raise ChunkDoesntExist("Chunk %s,%s doesn't exist" % (x,z))
 
         # Turn the Biomes array into a 16x16 numpy array
